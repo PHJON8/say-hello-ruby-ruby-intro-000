ae8e3de9188dd4edd3af8d54010f2a5c33ba7b37
Jo Onuma <joimportant@yahoo.co.uk>
2019-08-29 13:07:27 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -7,6 +7,9 @@ describe "say_hello" do
     say_hello("Kent Beck")
   end
 
+ def say_hello(name = "Ruby Programmer")
+   puts "Hello, #{name}."
+ end
   it 'defaults to Ruby Programmer when no name is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
